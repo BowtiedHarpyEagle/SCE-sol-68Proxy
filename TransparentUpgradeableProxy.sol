4308bd958716518a8818e9a624c37cef2fc1d782
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-09-28 10:20:01 -0500

    add BuggyProxy contract

--- TransparentUpgradeableProxy.sol
+++ TransparentUpgradeableProxy.sol
@@ -33,3 +33,30 @@ contract CounterV2 {
         
     }
 }
+
+contract BuggyProxy {
+    address public implementation;
+    address public admin;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function _delegate() private {
+        (bool ok, bytes memory res ) = implementation.delegatecall(msg.data);
+        require(ok, "delegatecall failed");
+    }
+
+    fallback() external payable {
+        _delegate();
+    }
+
+    receive() external payable {
+        _delegate();
+    }
+
+    function upgradeTo(address _implementation) external {
+        require(msg.sender == admin, "not authorised");
+        implementation = _implementation;
+    }
+}
