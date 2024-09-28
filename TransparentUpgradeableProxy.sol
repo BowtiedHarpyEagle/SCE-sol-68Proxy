f41cb634ca39b4ccfb3f050b8215ef7b2fb7f6d0
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-09-28 09:54:00 -0500

    add counter contracts V1 V2

--- TransparentUpgradeableProxy.sol
+++ TransparentUpgradeableProxy.sol
@@ -9,3 +9,27 @@ pragma solidity ^0.8.13;
 // -Separate user-admin interfaces 
 // -Proxy admin
 // -Demo
+
+contract CounterV1 {
+    uint public count;
+    
+    function inc() external {
+        count += 1;
+
+    }
+
+}
+
+contract CounterV2 {
+    uint public count;
+    
+    function inc() external {
+        count += 1;
+
+    }
+
+    function dec() external {
+        count -= 1;
+        
+    }
+}
