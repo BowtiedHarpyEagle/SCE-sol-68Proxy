da567898bf23ae33c1dc2b2be68051e1af21fff9
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-09-28 09:47:02 -0500

    add pragma and comments

---
+++ TransparentUpgradeableProxy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// Transparent upgradeable proxy pattern 
+// Topics
+// -Intro (wrong way) 
+// -Return Data From Fallback
+// -Storage for implementation and admin 
+// -Separate user-admin interfaces 
+// -Proxy admin
+// -Demo
